7389173b01df9095b9ede1ef8012cd061ffb49b8
meow <johndoe@example.com>
2026-03-09 23:56:20 +0700

    обработка ошибок для загрузки услуг

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,11 @@ const API_URL = 'http://127.0.0.1:8000/api';
 document.addEventListener('DOMContentLoaded', async () => {
     const servicesList = document.getElementById('servicesList');
     const serviceSelect = document.getElementById('service');
+    if (serviceSelect) {
+        try {
+            
+        } catch (error) {
+            console.error('Ошибка загрузки услуг:', error);
+        }
+    }
 });
