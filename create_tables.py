b51ccb68a982f7cb2d950d6fdac10c6be7de5b46
poc <johndoe@example.com>
2026-03-09 13:47:39 +0700

    бд

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,5 @@
 from app.database import engine, Base
 
-# Создаём все таблицы
+print("Создание таблиц в PostgreSQL...")
 Base.metadata.create_all(bind=engine)
-
 print("Таблицы успешно созданы!")
-print("База данных: nailmeow.db")
