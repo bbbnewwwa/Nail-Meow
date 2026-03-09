1c344896970262ac703f575b77972cea3257067b
poc <johndoe@example.com>
2026-03-09 14:12:53 +0700

    утиоиты

--- utils/logger.py
+++ utils/logger.py
@@ -1,28 +1,15 @@
 import logging
-import sys
-from pathlib import Path
 
-log_dir = Path("logs")
-log_dir.mkdir(exist_ok=True)
-
-logger = logging.getLogger("nailmeow")
-logger.setLevel(logging.INFO)
-
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
-
-file_handler = logging.FileHandler(log_dir / "app.log", encoding='utf-8')
-file_handler.setFormatter(formatter)
-file_handler.setLevel(logging.INFO)
-ь
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setFormatter(formatter)
-console_handler.setLevel(logging.INFO)
-
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
-
-def get_logger():
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    
     return logger
