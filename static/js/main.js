87b361534b7ecb9761b144a04360135bdafb4fb6
meow <johndoe@example.com>
2026-03-09 23:55:39 +0700

    ссылки на элементы и формы (примерн)

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 const API_URL = 'http://127.0.0.1:8000/api';
 
 document.addEventListener('DOMContentLoaded', async () => {
-    
+    const servicesList = document.getElementById('servicesList');
+    const serviceSelect = document.getElementById('service');
 });
