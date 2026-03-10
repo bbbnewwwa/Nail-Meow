9d5d9a850059d908476a1afa1f4176d648d0e976
poc <johndoe@example.com>
2026-03-10 20:10:28 +0700

    логгер

--- utils/logger.py
+++ utils/logger.py
@@ -1,15 +1,4 @@
 import logging
 
-def get_logger(name: str) -> logging.Logger:
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    
-    if not logger.handlers:
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-        )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-    
-    return logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
