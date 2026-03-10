5726d4607e37938287a5e9918f76025fd3cadb19
poc <johndoe@example.com>
2026-03-10 20:07:38 +0700

    бот мейн

--- bot/main.py
+++ bot/main.py
@@ -10,20 +10,13 @@ from handlers import start, contacts
 logging.basicConfig(level=logging.INFO)
 
 async def main():
-    bot = Bot(
-        token=settings.BOT_TOKEN,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-    )
-    
+    bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
     
     dp.include_router(start.router)
     dp.include_router(contacts.router)
     
-    print("Бот Nail Meow запущен...")
-    print(f"API URL: {settings.API_URL}")
-    print("Для остановки нажмите Ctrl+C")
-    
+    print(" Бот Nail Meow запущен...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
